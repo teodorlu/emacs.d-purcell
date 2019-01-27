76d8fc2cbfb744f696e33c78732aba16665831c8
Teodor Heggelund <teodor@teodorheggelund.com>
2019-01-27 22:00:18 +0100

    Add my own hook
    For the win!

---
+++ lisp/init-preload-local.el
@@ -0,0 +1,12 @@
+;;; Teodorstuff -- where I can put my own configuration
+;;; Commentary:
+;; Making Purcell's config a fit for myself.
+;; This is my personal file.  Can I do stuff here?
+
+;;; Code:
+
+(message "Hello, there! Looks like it was simple to write Emacs lisp here.")
+(message "Check the *messages* buffer, will you.")
+
+(provide 'init-preload-local)
+;;; init-preload-local.el ends here
